7b4175b28ad44513e0ccaa70b0318c8f68c35e25
toli18 <toli_18@abv.bg>
2025-12-10 19:08:27 +0200

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,9 @@
 from backend.app.seed import seed_clubs
+from backend.app.seed.seed_drills import seed_drills
 
 from pathlib import Path
 from alembic.config import Config
+
 
 BASE_DIR = Path(__file__).resolve().parents[2]  # стига до root
 alembic_cfg = Config(str(BASE_DIR / "alembic.ini"))
