a7e02c607fc79e8720b8a436dca2373ffc8da544
toli18 <toli_18@abv.bg>
2025-12-10 22:04:52 +0200

    Create 0003_add_country_to_clubs.py

---
+++ backend/app/migrations/versions/0003_add_country_to_clubs.py
@@ -0,0 +1,18 @@
+"""Add country column to clubs table"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# Revision identifiers
+revision = "0003_add_country_to_clubs"
+down_revision = "0002_add_users_table"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("clubs", sa.Column("country", sa.String(255)))
+
+
+def downgrade():
+    op.drop_column("clubs", "country")
