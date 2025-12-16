f4f3f7dcef04a05dfa8360d1e8e988550d90b47b
toli18 <toli_18@abv.bg>
2025-12-16 13:30:09 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,17 +2,16 @@ from fastapi import FastAPI
 
 from backend.app.routers import auth, users, clubs, drills
 from backend.app.init_db import init_db
-from backend.app.routers import drills
-
 
 app = FastAPI(title="Volley Platform API")
+
 
 @app.on_event("startup")
 def on_startup():
     init_db()
 
+
 app.include_router(auth, prefix="/auth", tags=["Auth"])
 app.include_router(users, prefix="/users", tags=["Users"])
 app.include_router(clubs, prefix="/clubs", tags=["Clubs"])
-app.include_router(drills, prefix="/drills", tags=["Drills"])
 app.include_router(drills.router)
